b7de84c52a64ee85e0186edc4bdc4562c1423ab1
熊荣亮 <xiongrl@ucweb.com>
2013-06-03 14:37:12 +0800

    add eap

--- src/play/cache/CacheFor.java
+++ src/play/cache/CacheFor.java
@@ -11,10 +11,22 @@ import java.lang.annotation.Target;
  * <p>If a time is not specified, the results will be cached for 1 hour by default.
  *
  * <p>Example: <code>@CacheFor("1h")</code>
+ * 用于action做缓存,详见 
+ * play.mvc.ActionInvoker.invoke(Request, Response)
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface CacheFor {
+	//缓存的时间
     String value() default "1h";
+    //作为缓存的key
     String id() default "";
+    /**
+     * action缓存可以，实现方式:
+     *  cacheKey = actionMethod.getAnnotation(CacheFor.class).id();
+     *  if ("".equals(cacheKey)) {
+     *       cacheKey = "urlcache:" + request.url + request.querystring;
+     *   }
+     *   详见:play.mvc.ActionInvoker.invoke(Request, Response)
+     */
 }
